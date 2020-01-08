c3542024fa450883503e3d7b4a6e0852900bf54f
LuiGee3471 <yjs3475@gmail.com>
2020-01-08 23:43:44 +0900

    Finished seed, started view

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
+from django.utils import timezone
 from django.shortcuts import render
 
 # Create your views here.
+def all_rooms(request):
+    now = timezone.now()
+    hungry = True
+    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
